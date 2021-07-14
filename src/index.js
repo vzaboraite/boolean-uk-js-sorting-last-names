0e208b20ee5a92ada441aaaa9d16c2fdee9ca5bc
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-14 18:12:35 +0100

    Finish main exercise.

--- src/index.js
+++ src/index.js
@@ -7,3 +7,39 @@
 // - Output the position of your name
 // - Uppercase all names
 // - Output uppercased names
+
+const lastNames = ["Mason", "Marcora", "Rico", "Neves", "Ivanov"];
+
+// User input
+const newLastName = "Zaboraite"; //prompt("What is your last name?");
+console.log(newLastName);
+
+// Add new name to the end of an array
+const addLastNewName = lastNames.push(newLastName);
+console.log(lastNames);
+
+// Sort names alphabetically
+const sortedLastNames = lastNames.sort();
+console.log(sortedLastNames);
+
+// Find the position of my name
+// const namePosition = names.indexOf("Zaboraite");
+// console.log(namePosition);
+
+let nameLastPosition = null;
+
+for (let i = 0; i < lastNames.length; i++) {
+  if (lastNames[i] === "Zaboraite") {
+    nameLastPosition = i;
+  }
+}
+
+console.log(nameLastPosition);
+
+// Uppercase all the names
+let uppercaseLastName = null;
+
+for (let i = 0; i < lastNames.length; i++) {
+  uppercaseLastName = lastNames[i].toUpperCase();
+  console.log(uppercaseLastName);
+}
