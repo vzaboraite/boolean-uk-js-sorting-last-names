f8d8696ff6874db4cc97736aa13c0c6289089261
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-14 15:29:55 +0100

    Add pseudocode.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// - Create an array with the following names: Mason, Marcora, Rico, Neves, Ivanov
+// - Ask user to input last name / use prompt()
+// - Add last name to the array
+// - Sort the names alphabetically
+// - Output sorted array
+// - Find the position of your name in the sorted array
+// - Output the position of your name
+// - Uppercase all names
+// - Output uppercased names
